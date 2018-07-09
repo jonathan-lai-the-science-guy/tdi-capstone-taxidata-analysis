59651159e4a341b96617984e6895d2743993622f
JLai <jonathan.lai.the.science.guy@gmail.com>
2018-07-09 11:56:56 +0000

    Minor updated to GridLib

--- Custom_Libraries/GridLib.py
+++ Custom_Libraries/GridLib.py
@@ -19,13 +19,13 @@ class BaseGrid:
     """
     BaseGrid : Class to setup a geographically aware grid
     """
-    EAST_BOUND = -73.750
-    SOUTH_BOUND = 40.550
-    WEST_BOUND = -74.150
-    NORTH_BOUND = 40.900
+#    self.EAST_BOUND = -73.750
+#    self.SOUTH_BOUND = 40.550
+#    self.WEST_BOUND = -74.150
+#    self.NORTH_BOUND = 40.900
 
-    LON_CENTER = -73.95
-    LAT_CENTER = 40.70
+#    LON_CENTER = -73.95
+#    LAT_CENTER = 40.70
 
     # Dummy declarations
     # self.xeLiquorOrig = np.array([])
@@ -40,29 +40,35 @@ class BaseGrid:
                  WEST_BOUND = -74.150,\
                  NORTH_BOUND = 40.900):
 
+        # Initialize boundaries
+        self.EAST_BOUND = EAST_BOUND
+        self.SOUTH_BOUND = SOUTH_BOUND
+        self.WEST_BOUND = WEST_BOUND
+        self.NORTH_BOUND = NORTH_BOUND
+
         # Cleanup everything
         gc.collect()
 
         # Check to make sure that the input is okay:
-        assert(EAST_BOUND > WEST_BOUND)
-        assert(NORTH_BOUND > SOUTH_BOUND)
+        assert(self.EAST_BOUND > self.WEST_BOUND)
+        assert(self.NORTH_BOUND > self.SOUTH_BOUND)
         assert(resolution > 0)
-        assert(EAST_BOUND <= 180. and WEST_BOUND >= -180.)
-        assert(NORTH_BOUND <= 90. and SOUTH_BOUND >= -90.)
+        assert(self.EAST_BOUND <= 180. and self.WEST_BOUND >= -180.)
+        assert(self.NORTH_BOUND <= 90. and self.SOUTH_BOUND >= -90.)
 
         # Precompute the dimensionality of the geographical space
-        LON_BOUND = EAST_BOUND - WEST_BOUND
-        LAT_BOUND = NORTH_BOUND - SOUTH_BOUND 
+        LON_BOUND = self.EAST_BOUND - self.WEST_BOUND
+        LAT_BOUND = self.NORTH_BOUND - self.SOUTH_BOUND 
         
         # Precompute the center of hte geographical space
-        LON_CENTER = (EAST_BOUND + WEST_BOUND)/2.0
-        LAT_CENTER = (SOUTH_BOUND + NORTH_BOUND)/2.0
+        self.LON_CENTER = (self.EAST_BOUND + self.WEST_BOUND)/2.0
+        self.LAT_CENTER = (self.SOUTH_BOUND + self.NORTH_BOUND)/2.0
 
         # Compute a rough bounding box for the grid space
-        COORDS_1 = (LAT_CENTER + 0.5,LON_CENTER)
-        COORDS_2 = (LAT_CENTER - 0.5,LON_CENTER)
-        COORDS_3 = (LAT_CENTER,LON_CENTER + 0.5)
-        COORDS_4 = (LAT_CENTER,LON_CENTER - 0.5)
+        COORDS_1 = (self.LAT_CENTER + 0.5,self.LON_CENTER)
+        COORDS_2 = (self.LAT_CENTER - 0.5,self.LON_CENTER)
+        COORDS_3 = (self.LAT_CENTER,self.LON_CENTER + 0.5)
+        COORDS_4 = (self.LAT_CENTER,self.LON_CENTER - 0.5)
 
         # Compute the conversion of degrees to 
         latDeg2m = (geopy.distance.vincenty(COORDS_1, COORDS_2).km)
@@ -77,8 +83,8 @@ class BaseGrid:
         lonBins = int(np.round(LON_BOUND * lonDeg2m,2) * 33)
 
         # Compute iterable range for the space
-        latRange = np.linspace(SOUTH_BOUND,NORTH_BOUND,latBins)
-        lonRange = np.linspace(WEST_BOUND,EAST_BOUND,lonBins)
+        latRange = np.linspace(self.SOUTH_BOUND,self.NORTH_BOUND,latBins)
+        lonRange = np.linspace(self.WEST_BOUND,self.EAST_BOUND,lonBins)
         
         # [ JLai ] Works but needs to be rewritten
         histLiquor,self.xeLiquor,self.yeLiquor = np.histogram2d([0],[0],\
@@ -104,6 +110,15 @@ class BaseGrid:
         """
         Returns a single grid index given a tuple of longitude and latitude
         """
+
+        if(lon < self.WEST_BOUND):
+            lon = self.WEST_BOUND
+        if(lon > self.EAST_BOUND):
+            lon = self.EAST_BOUND
+        if(lat < self.SOUTH_BOUND):
+            lat = self.SOUTH_BOUND
+        if(lat > self.NORTH_BOUND):
+            lat = self.NORTH_BOUND
 
         # This is SLOW code
         X = np.abs(self.xeLiquor-lat).argmin()
